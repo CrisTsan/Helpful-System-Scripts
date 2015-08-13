e3003b8efd8457d50903f5d3196fcc41800bc11f
Chris Tsanikidis <mr.tsanikidis@gmail.com>
2015-08-13 22:29:37 +0300

    Delete clean_my_desktop.sh

--- clean_my_desktop.sh
+++
@@ -1,6 +0,0 @@
-cd ~/Desktop
-
-echo "\t\t\t\t\t\t\t\t Cleaning Desktop Log"
-find /Users/SubtleX/Desktop -mtime +10 -exec mv -v >> .mylog "{}" /Users/SubtleX/Documents/_DESKTOP/  \;
-
-cat .mylog | tail -10
